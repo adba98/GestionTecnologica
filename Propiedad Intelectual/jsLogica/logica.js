302ee10e96fbd91a9201d0df051e59611f4ae311
adba98 <oscar.adba@gmail.com>
2020-03-15 22:00:07 -0500

    Ruleta

--- Propiedad Intelectual/jsLogica/logica.js
+++ Propiedad Intelectual/jsLogica/logica.js
@@ -1,14 +1,59 @@
 var ruleta;
 var winningSegment;
-var distnaciaX = 15;
-var distnaciaY = 50;
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var img = new Image();
-img.src = "img/pi.png";
 
+var puntaje = 0;
+var vidas = 3;
+var preguntasA =[
+    "1A","2A","3A"
+];
 
-ctx.drawImage(img, 100, 50);
+var preguntasB =[
+    "1B","2B","3B"
+];
+
+var preguntasC =[
+    "1C","2C","3C"
+];
+
+var preguntasD =[
+    "1D","2D","3D"
+];
+
+var preguntasE =[
+    "1E","2E","3E"
+];
+
+var respuestasA = [
+    ["a1A","b1A","c1A","d1A"],
+    ["a2A","b2A","c2A","d2A"],
+    ["a3A","b3A","c3A","d3A"]
+];
+
+var respuestasB = [
+    ["a1","b1","c1","d1"],
+    ["a2","b2","c2","d2"],
+    ["a3","b3","c3","d3"]
+];
+
+var respuestasC = [
+    ["a1","b1","c1","d1"],
+    ["a2","b2","c2","d2"],
+    ["a3","b3","c3","d3"]
+];
+
+var respuestasD = [
+    ["a1","b1","c1","d1"],
+    ["a2","b2","c2","d2"],
+    ["a3","b3","c3","d3"]
+];
+
+var respuestasE = [
+    ["a1","b1","c1","d1"],
+    ["a2","b2","c2","d2"],
+    ["a3","b3","c3","d3"]
+];
 
 function DibujarRuleta() {
     ruleta = new Winwheel({
@@ -21,15 +66,28 @@ function DibujarRuleta() {
         { 'fillStyle': '#F7DC6F', 'text': 'Tema d'},
         { 'fillStyle': '#EC7063', 'text': 'Tema e'},
         ],
-        'animation' : {
-            'type': 'spinToStop',
-            'duration' : 5,
-            'spins': 15,
+        'animation' :{
+            'type'          : 'spinToStop',
             'callbackFinished': 'Mensaje()',
-            'callbackAfter': 'DibujarTriangulo()' 
-        }
+            'callbackAfter': 'DibujarTriangulo()' ,
+            'duration'      : 10,
+            'spins'         : 5,
+            'callbackSound' : playSound(),
+            'soundTrigger'  : 'pin'
+        },
+        'pins' :{'number' : 16}
     });
     DibujarTriangulo();
+}
+
+let audio = new Audio("img/misc333.mp3");
+
+function playSound(){
+    alert("hola")
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+    audio.muted="muted"
 }
 
 function Mensaje() {
@@ -37,18 +95,14 @@ function Mensaje() {
     swal({
         title: " ¡ " + winningSegment.text + " !",
         imageUrl: "img/pi.png",
-        showCancelButton: true,
-        confirmButtonColor: "#e74c3c",
-        confirmButtonText: "Ok,Reiniciar",
-        cancelButtonText: "Quitar elemento",
-        closeOnConfirm: true,
-        closeOnCancel: true
+        confirmButtonColor: "#28B463",
+        confirmButtonText: "Responder",
+        closeOnConfirm: true
     },
         function (isConfirm) {
             if (isConfirm) {
-                document.getElementById('puntaje').innerHTML = "H";
-            } else {
-
+                document.getElementById('girar').disabled= true;
+                temaRuleta(winningSegment.text)
             }
             ruleta.stopAnimation(false);
             ruleta.rotationAngle = 0;
@@ -61,15 +115,90 @@ function Mensaje() {
 function DibujarTriangulo() {
     ctx = ruleta.ctx;
     ctx.strokeStyle = 'navy';
-    ctx.fillStyle = '#000000';
-    ctx.lineWidth = 2;
+    ctx.fillStyle = '#424949';
     ctx.beginPath();
-    ctx.moveTo(distnaciaX + 170, distnaciaY + 5);
-    ctx.lineTo(distnaciaX + 230, distnaciaY + 5);
-    ctx.lineTo(distnaciaX + 200, distnaciaY + 40);
-    ctx.lineTo(distnaciaX + 171, distnaciaY + 5);
+    ctx.lineTo(210, 5);
+    ctx.lineTo(200, 50);
+    ctx.lineTo(185, 5);
     ctx.stroke();
     ctx.fill();
 }
+
+function temaRuleta(tema){
+    switch (tema) {
+        case 'Tema a':
+            generarPregunta(preguntasA, respuestasA)
+        break;
+        case 'Tema b':
+            generarPregunta(preguntasB, respuestasB)
+        break;
+        case 'Tema c':
+            generarPregunta(preguntasC, respuestasC)
+        break;
+        case 'Tema d':
+            generarPregunta(preguntasD, respuestasD)
+        break;
+        case 'Tema e':
+            generarPregunta(preguntasE, respuestasE)
+        break;
+    }
+
+}
+
+function generarPregunta(preguntas,respuestas){
+    var indice_aleatorio = Math.floor(Math.random()*preguntas.length);
+    var respuestas_posibles = respuestas[indice_aleatorio];
+    var posiciones = [0,1,2,3];
+    var respuestas_reordenadas = [];
+
+    var acierto = false;
+    for(i in respuestas_posibles){
+        var posicion_aleatoria = Math.floor(Math.random()*posiciones.length);
+        if(posicion_aleatoria==0 && acierto == false){
+            indicie_respuesta_correcta = i;
+            acierto = true;
+        }
+        respuestas_reordenadas[i] = respuestas_posibles[posiciones[posicion_aleatoria]];
+        posiciones.splice(posicion_aleatoria, 1);
+    }
+
+    var txt_respuestas="";
+    for(i in respuestas_reordenadas){
+        txt_respuestas += '<input type="radio" name="pp" value="'+i+'"><label>'+
+        respuestas_reordenadas[i]+'</label>';
+    }
+
+    
+    document.getElementById("respuestas").innerHTML = txt_respuestas;
+    document.getElementById("pregunta").innerHTML = preguntas[indice_aleatorio];
+    document.getElementById('btnResp').innerHTML = '<input id="resp" class="btn btn-color btn-success" type="button" value="Responder" onclick="comprobar();" />';
+
+}
+
+function comprobar(){
+	var respuesta = $("input[type=radio]:checked").val();
+	if(respuesta == indicie_respuesta_correcta){
+        puntaje = puntaje + 10
+        document.getElementById('puntaje').innerHTML = "<h3>" + puntaje +"</h3>";
+	}else{
+        vidas = vidas - 1
+        if( vidas == 0){
+            swal({
+                title: " ¡ Ha Perdido !\n Puntaje: "+ puntaje,
+                imageUrl: "img/pi.png",
+                confirmButtonColor: "#e74c3c",
+                confirmButtonText: "Reiniciar",
+                closeOnConfirm: true
+            },
+                function (isConfirm) {
+                    location.reload();
+                });
+        }
+        document.getElementById('intentos').innerHTML = "<h3>" + vidas +"</h3>";
+    }
+    document.getElementById('girar').disabled= false;
+    document.getElementById('resp').disabled= true;
+}
+
 
 DibujarRuleta();
